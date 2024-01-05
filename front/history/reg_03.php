67379f7c4bdc1ec7fe45fb4a29701773df03687d
dean5201314 <smile90624@gmail.com>
2024-01-05 09:38:55 +0800

    完成主題內容消息區點選切換功能

--- front/history/reg_03.php
+++ front/history/reg_03.php
@@ -38,7 +38,13 @@
         // console.log(user);
         if (user.acc != '' && user.pw != '' && user.pw2 != '' && user.email != '') {
             if (user.pw == user.pw2) {
-                
+                $.post("./api/chk_acc.php",{acc:user.acc},(res)=>{
+                    if(parseInt(res)==1){
+                        alert("帳號重複")
+                    }else{
+
+                    }
+                })
             } else {
                 alert("密碼錯誤")
             }
